a087a27fdcde4f1468c849a3e84688488cf40978
Federico Genaro <fedegen14@gmail.com>
2025-09-22 21:40:58 -0300

    ops: create generate_compose.sh and config.yaml

---
+++ generate_compose.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+CONFIG_FILE="${1:-config.yaml}"
+OUTPUT_FILE="${2:-docker-compose.yaml}"
+
+# header
+echo "name: tp1
+services:" > "$OUTPUT_FILE"
+
+while IFS= read -r line; do
+    [[ "$line" =~ ^services ]] && continue
+
+    echo "$line"
+
+    IFS=":" read -r service replicas <<< "$line"
+    service=$(echo "$service" | xargs)
+    replicas=$(echo "$replicas" | xargs)
+
+    for i in $(seq 1 "$replicas"); do
+        if [ "$replicas" -eq 1 ]; then
+            name="$service"
+        else
+            name="${service}${i}"
+        fi
+        echo "  ${name}:
+            container_name: ${name}
+            build:
+              context: ./src/${service}
+              dockerfile: Dockerfile
+            image: ${service}:latest
+            networks:
+              - tp_net
+            volumes:
+              - ./${service}/config.yaml:/config.yaml
+          " >> "$OUTPUT_FILE"
+    done
+done < "$CONFIG_FILE"
+
+# networks
+echo "networks:
+  testing_net:
+    driver: default" >> "$OUTPUT_FILE"
