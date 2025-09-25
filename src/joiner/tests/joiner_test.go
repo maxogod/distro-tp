50df2e1d23a5f1a1a026eeb313f6ec6bafaff7e0
Federico Genaro <fedegen14@gmail.com>
2025-09-25 14:48:41 -0300

    test: add test for joiner persist reference batches for users dataset

--- src/joiner/tests/joiner_test.go
+++ src/joiner/tests/joiner_test.go
@@ -11,6 +11,7 @@ import (
 )
 
 const datasetTypeMenuItems = 0
+const datasetTypeUsers = 2
 
 func TestJoinerPersistReferenceBatchesMenuItems(t *testing.T) {
 	rabbitURL := "amqp://guest:guest@localhost:5672/"
@@ -37,4 +38,33 @@ func TestJoinerPersistReferenceBatchesMenuItems(t *testing.T) {
 	expectedFile := filepath.Join(storeDir, fmt.Sprintf("%s.csv", datasetName))
 
 	helper.AssertFileContainsPayloads(t, expectedFile, csvPayloads)
+}
+
+func TestJoinerPersistReferenceBatchesUsers(t *testing.T) {
+	rabbitURL := "amqp://guest:guest@localhost:5672/"
+	refQueueName := "test_users"
+	storeDir := t.TempDir()
+	datasetName := "users"
+
+	j := helper.StartJoiner(t, rabbitURL, storeDir, refQueueName)
+	defer j.Stop()
+
+	pub, err := middleware.NewQueueMiddleware(rabbitURL, refQueueName)
+	assert.NoError(t, err)
+	defer func() {
+		_ = pub.Delete()
+		_ = pub.Close()
+	}()
+
+	csvPayloads := [][]byte{
+		[]byte("1,female,1970-04-22,2023-07-01 08:13:07\n"),
+		[]byte("581,male,2004-06-13,2023-08-01 09:39:30\n"),
+	}
+	helper.SendReferenceBatches(t, pub, csvPayloads, datasetTypeUsers)
+
+	expectedFile := filepath.Join(storeDir, fmt.Sprintf("%s_202307.csv", datasetName))
+	anotherExpectedFile := filepath.Join(storeDir, fmt.Sprintf("%s_202308.csv", datasetName))
+
+	helper.AssertFileContainsPayloads(t, expectedFile, [][]byte{csvPayloads[0]})
+	helper.AssertFileContainsPayloads(t, anotherExpectedFile, [][]byte{csvPayloads[1]})
 }
