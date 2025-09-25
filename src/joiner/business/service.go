314864759399b32e11965e6e7680df26d0b01f3d
Federico Genaro <fedegen14@gmail.com>
2025-09-25 19:33:55 -0300

    refactor: reference data handling into utils

--- src/joiner/business/service.go
+++ src/joiner/business/service.go
@@ -2,27 +2,14 @@ package business
 
 import (
 	"fmt"
-	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/maxogod/distro-tp/src/common/middleware"
-	"github.com/maxogod/distro-tp/src/common/utils"
 	"github.com/maxogod/distro-tp/src/joiner/config"
 	"github.com/maxogod/distro-tp/src/joiner/protocol"
+	joinerUtils "github.com/maxogod/distro-tp/src/joiner/utils"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"google.golang.org/protobuf/proto"
 )
-
-var datasetNames = map[int32]string{
-	0: "menu_items",
-	1: "stores",
-	2: "users",
-}
-
-const datasetTypeUsers = 2
-const separatorBatchData = ","
-const registeredAtColumn = 3
 
 type Joiner struct {
 	config              *config.Config
@@ -66,7 +53,7 @@ func (j *Joiner) handleMessage(msg *amqp.Delivery) {
 
 	switch payload := refQueueMsg.Payload.(type) {
 	case *protocol.ReferenceQueueMessage_ReferenceBatch:
-		j.cacheReferenceData(msg, payload.ReferenceBatch)
+		joinerUtils.StoreReferenceData(j.config.StorePath, msg, payload.ReferenceBatch)
 	case *protocol.ReferenceQueueMessage_Done:
 		err := j.startDataConsumer(msg)
 		if err != nil {
@@ -77,21 +64,6 @@ func (j *Joiner) handleMessage(msg *amqp.Delivery) {
 		// Unknown message
 		_ = msg.Nack(false, false)
 	}
-}
-
-func (j *Joiner) cacheReferenceData(referenceData *amqp.Delivery, batch *protocol.ReferenceBatch) {
-	datasetFilename, ok := getDatasetFilename(j.config.StorePath, batch)
-	if !ok {
-		_ = referenceData.Nack(false, false)
-		return
-	}
-
-	if err := utils.AppendToCSVFile(datasetFilename, batch.Payload); err != nil {
-		_ = referenceData.Nack(false, true)
-		return
-	}
-
-	_ = referenceData.Ack(false)
 }
 
 func (j *Joiner) startDataConsumer(msg *amqp.Delivery) error {
@@ -131,39 +103,4 @@ func (j *Joiner) Stop() error {
 
 func (j *Joiner) handleTaskType3(msg amqp.Delivery) {
 
-}
-
-func getDatasetFilename(storePath string, batch *protocol.ReferenceBatch) (string, bool) {
-	datasetName, ok := datasetNames[batch.DatasetType]
-	if !ok {
-		return "", false
-	}
-
-	var datasetFilename string
-
-	if batch.DatasetType == datasetTypeUsers {
-		year, month, err := getYearMonth(batch.Payload)
-		if err != nil {
-			return "", false
-		}
-		datasetFilename = filepath.Join(storePath, fmt.Sprintf("%s_%d%s.csv", datasetName, year, month))
-	} else {
-		datasetFilename = filepath.Join(storePath, fmt.Sprintf("%s.csv", datasetName))
-	}
-
-	return datasetFilename, ok
-}
-
-func getYearMonth(batchPayload []byte) (int, string, error) {
-	row := string(batchPayload)
-	cols := strings.Split(row, separatorBatchData)
-
-	dateStr := strings.TrimSpace(cols[registeredAtColumn])
-
-	t, err := time.Parse(time.DateTime, dateStr)
-	if err != nil {
-		return 0, "", err
-	}
-
-	return t.Year(), fmt.Sprintf("%02d", int(t.Month())), nil
 }
---
+++ src/joiner/utils/utils.go
@@ -0,0 +1,72 @@
+package utils
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/maxogod/distro-tp/src/common/utils"
+	"github.com/maxogod/distro-tp/src/joiner/protocol"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var datasetNames = map[int32]string{
+	0: "menu_items",
+	1: "stores",
+	2: "users",
+}
+
+const datasetTypeUsers = 2
+const separatorBatchData = ","
+const registeredAtColumn = 3
+
+func StoreReferenceData(storePath string, referenceData *amqp.Delivery, batch *protocol.ReferenceBatch) {
+	datasetFilename, ok := getDatasetFilename(storePath, batch)
+	if !ok {
+		_ = referenceData.Nack(false, false)
+		return
+	}
+
+	if err := utils.AppendToCSVFile(datasetFilename, batch.Payload); err != nil {
+		_ = referenceData.Nack(false, true)
+		return
+	}
+
+	_ = referenceData.Ack(false)
+}
+
+func getDatasetFilename(storePath string, batch *protocol.ReferenceBatch) (string, bool) {
+	datasetName, ok := datasetNames[batch.DatasetType]
+	if !ok {
+		return "", false
+	}
+
+	var datasetFilename string
+
+	if batch.DatasetType == datasetTypeUsers {
+		year, month, err := getYearMonth(batch.Payload)
+		if err != nil {
+			return "", false
+		}
+		datasetFilename = filepath.Join(storePath, fmt.Sprintf("%s_%d%s.csv", datasetName, year, month))
+	} else {
+		datasetFilename = filepath.Join(storePath, fmt.Sprintf("%s.csv", datasetName))
+	}
+
+	return datasetFilename, ok
+}
+
+func getYearMonth(batchPayload []byte) (int, string, error) {
+	row := string(batchPayload)
+	cols := strings.Split(row, separatorBatchData)
+
+	dateStr := strings.TrimSpace(cols[registeredAtColumn])
+
+	t, err := time.Parse(time.DateTime, dateStr)
+	if err != nil {
+		return 0, "", err
+	}
+
+	return t.Year(), fmt.Sprintf("%02d", int(t.Month())), nil
+}
