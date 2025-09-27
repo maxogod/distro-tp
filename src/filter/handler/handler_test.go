8bfcbb2c3d623860b35f8ac9d167fb708d4bf247
sev <santisevitz@hotmail.com>
2025-09-27 16:47:00 -0300

    refactor: implementation of handler with proper tests and configurations

---
+++ src/filter/handler/handler_test.go
@@ -0,0 +1,137 @@
+package handler_test
+
+import (
+	"coffee-analisis/src/common/models"
+	"coffee-analisis/src/filter/business"
+	"coffee-analisis/src/filter/handler"
+	"coffee-analisis/src/filter/test/mock"
+	"testing"
+)
+
+var taskConfig = &handler.TaskConfig{
+	FilterYearFrom:       2024,
+	FilterYearTo:         2025,
+	BusinessHourFrom:     6,
+	BusinessHourTo:       11,
+	TotalAmountThreshold: 75.0,
+}
+
+func TestNewTaskHandler(t *testing.T) {
+	filterService := business.NewFilterService()
+	th := handler.NewTaskHandler(filterService, taskConfig)
+
+	if th == nil {
+		t.Fatal("NewTaskHandler() returned nil")
+	}
+}
+
+func TestTaskHandler_HandleTransactionTasksCorrectly(t *testing.T) {
+	filterService := business.NewFilterService()
+	th := handler.NewTaskHandler(filterService, taskConfig)
+
+	tests := []struct {
+		name        string
+		taskType    models.TaskType
+		payload     []models.Transaction
+		expected    map[string]models.Transaction
+		errorString string
+	}{
+		{
+			name:     "T1 - Filter transactions by year between 2024-2025, business hours between 6-11 hs, and total amount > 75.0",
+			taskType: models.T1,
+			payload:  mock.MockTransactions,
+			expected: mock.MockTransactionsOutputT1,
+		},
+		{
+			name:     "T3 - Filter transactions by year between 2024-2025 and by business hours between 6-11 hs",
+			taskType: models.T3,
+			payload:  mock.MockTransactions,
+			expected: mock.MockTransactionsOutputT3,
+		},
+		{
+			name:     "T4 - Filter transactions by year between 2024-2025",
+			taskType: models.T4,
+			payload:  mock.MockTransactions,
+			expected: mock.MockTransactionsOutputT4,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := th.HandleTask(tt.taskType, tt.payload)
+
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+				return
+			}
+
+			if !compareTransactions(result.([]models.Transaction), tt.expected) {
+				t.Errorf("Results don't match expected.\nExpected: %+v\nGot: %+v", tt.expected, result)
+			}
+		})
+	}
+}
+
+func TestTaskHandler_HandleTransactionItemsTasksCorrectly(t *testing.T) {
+	filterService := business.NewFilterService()
+	th := handler.NewTaskHandler(filterService, taskConfig)
+
+	tests := []struct {
+		name        string
+		taskType    models.TaskType
+		payload     []models.TransactionItem
+		expected    map[string]models.TransactionItem
+		errorString string
+	}{
+		{
+			name:     "T2 - Filter transactions by year between 2024-2025",
+			taskType: models.T2,
+			payload:  mock.MockTransactionItems,
+			expected: mock.MockTransactionItemsOutputT2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := th.HandleTask(tt.taskType, tt.payload)
+
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+				return
+			}
+
+			if !compareTransactionItems(result.([]models.TransactionItem), tt.expected) {
+				t.Errorf("Results don't match expected.\nExpected: %+v\nGot: %+v", tt.expected, result)
+			}
+		})
+	}
+}
+
+func compareTransactions(actual []models.Transaction, expected map[string]models.Transaction) bool {
+	if len(expected) != len(actual) {
+		return false
+	}
+
+	for _, actual := range actual {
+		exp, exists := expected[actual.TransactionId]
+		if !exists || !actual.IsEqual(exp) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func compareTransactionItems(actual []models.TransactionItem, expected map[string]models.TransactionItem) bool {
+	if len(expected) != len(actual) {
+		return false
+	}
+
+	for _, actual := range actual {
+		exp, exists := expected[actual.TransactionId]
+		if !exists || !actual.IsEqual(exp) {
+			return false
+		}
+	}
+	return true
+}
--- src/filter/handler/task_handler.go
+++ src/filter/handler/task_handler.go
@@ -14,15 +14,17 @@ type TaskHandler struct {
 	FilterService *business.FilterService
 	// TODO: this should be replace with a protocol buffer that when given the task type, then process the payload insted of
 	// directly giving the task type and payload
-	taskHandlers map[models.TaskType]func(interface{}) (interface{}, error)
+	taskHandlers map[models.TaskType]func(any) (any, error)
+	TaskConfig   *TaskConfig
 }
 
-func NewTaskHandler() *TaskHandler {
+func NewTaskHandler(filterService *business.FilterService, taskConfig *TaskConfig) *TaskHandler {
 	th := &TaskHandler{
-		FilterService: business.NewFilterService(),
+		FilterService: filterService,
+		TaskConfig:    taskConfig,
 	}
 
-	th.taskHandlers = map[models.TaskType]func(interface{}) (interface{}, error){
+	th.taskHandlers = map[models.TaskType]func(any) (any, error){
 		models.T1: th.handleTaskType1,
 		models.T2: th.handleTaskType2,
 		models.T3: th.handleTaskType3,
@@ -32,7 +34,7 @@ func NewTaskHandler() *TaskHandler {
 	return th
 }
 
-func (th *TaskHandler) HandleTask(taskType models.TaskType, payload interface{}) (interface{}, error) {
+func (th *TaskHandler) HandleTask(taskType models.TaskType, payload any) (any, error) {
 	handler, exists := th.taskHandlers[taskType]
 	if !exists {
 		return nil, fmt.Errorf("unknown task type: %d", taskType)
@@ -42,50 +44,53 @@ func (th *TaskHandler) HandleTask(taskType models.TaskType, payload interface{})
 	return handler(payload)
 }
 
-func (th *TaskHandler) handleTaskType1(payload interface{}) (interface{}, error) {
-	log.Info("Handling Task Type 1 - Filter transactions by year 2023")
+func (th *TaskHandler) handleTaskType1(payload any) (any, error) {
+	log.Debug("Handling Task Type 1")
 
 	transactions, ok := payload.([]models.Transaction)
 	if !ok {
 		return nil, fmt.Errorf("task T1 expects a batch of Transactions, got %T", payload)
 	}
 
-	result := business.FilterByYearBetween(2023, 2024, transactions)
+	result := business.FilterByYearBetween(th.TaskConfig.FilterYearFrom, th.TaskConfig.FilterYearTo, transactions)
+	result = business.FilterByHourBetween(th.TaskConfig.BusinessHourFrom, th.TaskConfig.BusinessHourTo, result)
+	result = business.FilterByTotalAmountGreaterThan(th.TaskConfig.TotalAmountThreshold, result)
 	return result, nil
 }
 
-func (th *TaskHandler) handleTaskType2(payload interface{}) (interface{}, error) {
-	log.Info("Handling Task Type 2 - Filter transaction items by amount >= 50")
+func (th *TaskHandler) handleTaskType2(payload any) (any, error) {
+	log.Debug("Handling Task Type 2")
 
 	items, ok := payload.([]models.TransactionItem)
 	if !ok {
 		return nil, fmt.Errorf("task T2 expects a batch of Transaction Item, got %T", payload)
 	}
 
-	result := business.FilterByYearBetween(2023, 2024, items)
+	result := business.FilterByYearBetween(th.TaskConfig.FilterYearFrom, th.TaskConfig.FilterYearTo, items)
 	return result, nil
 }
 
-func (th *TaskHandler) handleTaskType3(payload interface{}) (interface{}, error) {
-	log.Info("Handling Task Type 3 - Filter transactions by business hours (9-17)")
+func (th *TaskHandler) handleTaskType3(payload any) (any, error) {
+	log.Debug("Handling Task Type 3")
 
 	transactions, ok := payload.([]models.Transaction)
 	if !ok {
 		return nil, fmt.Errorf("task T3 expects a batch of Transactions, got %T", payload)
 	}
 
-	result := business.FilterByHourBetween(9, 17, transactions)
+	result := business.FilterByYearBetween(th.TaskConfig.FilterYearFrom, th.TaskConfig.FilterYearTo, transactions)
+	result = business.FilterByHourBetween(th.TaskConfig.BusinessHourFrom, th.TaskConfig.BusinessHourTo, result)
 	return result, nil
 }
 
-func (th *TaskHandler) handleTaskType4(payload interface{}) (interface{}, error) {
-	log.Info("Handling Task Type 4 - Filter transactions by amount >= 100")
+func (th *TaskHandler) handleTaskType4(payload any) (any, error) {
+	log.Debug("Handling Task Type 4")
 
 	transactions, ok := payload.([]models.Transaction)
 	if !ok {
 		return nil, fmt.Errorf("task T4 expects a batch of Transactions, got %T", payload)
 	}
 
-	result := business.FilterByTotalAmountGreaterThan(100.0, transactions)
+	result := business.FilterByYearBetween(th.TaskConfig.FilterYearFrom, th.TaskConfig.FilterYearTo, transactions)
 	return result, nil
 }
