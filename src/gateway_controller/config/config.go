98b60c6998afbb196f5dd1331ff9d48f7cf57645
sev <santisevitz@hotmail.com>
2025-09-28 17:37:59 -0300

    feat: implementing basic task handler and service logic to gateway controller with tested features

--- src/gateway_controller/config/config.go
+++ src/gateway_controller/config/config.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	LogLevel string
+}
+
+func (c Config) String() string {
+	return fmt.Sprintf(
+		"LogLevel: %s",
+		c.LogLevel,
+	)
+}
+
+const CONFIG_FILE_PATH = "/config.yaml"
+
+func InitConfig() (*Config, error) {
+
+	v := viper.New()
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+
+	v.SetConfigFile(CONFIG_FILE_PATH)
+	if err := v.ReadInConfig(); err != nil {
+		return nil, errors.Wrapf(err, "failed to read config file %s", CONFIG_FILE_PATH)
+	}
+
+	config := &Config{
+		LogLevel: v.GetString("log.level"),
+	}
+
+	return config, nil
+}
