79623925aace1fa158a8c357e1256fcc5d398fa1
maxogod <maxi18wars@gmail.com>
2025-09-28 15:50:45 -0300

    feat: base for client and dockerfile

--- src/gateway/config/config.go
+++ src/gateway/config/config.go
@@ -1,1 +1,34 @@
 package config
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerHost string
+	ServerPort int
+}
+
+const CONFIG_FILE_PATH = "./config.yaml"
+
+func InitConfig() (*Config, error) {
+
+	v := viper.New()
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+
+	v.SetConfigFile(CONFIG_FILE_PATH)
+	if err := v.ReadInConfig(); err != nil {
+		return nil, errors.New("failed to read config file " + CONFIG_FILE_PATH)
+	}
+
+	config := &Config{
+		ServerHost: v.GetString("server.host"),
+		ServerPort: v.GetInt("server.port"),
+	}
+
+	return config, nil
+}
--- src/gateway/internal/client/client.go
+++ src/gateway/internal/client/client.go
@@ -1,1 +1,24 @@
 package client
+
+import (
+	"github.com/maxogod/distro-tp/src/common/logger"
+	"github.com/maxogod/distro-tp/src/gateway/config"
+)
+
+var log = logger.GetLogger()
+
+type client struct {
+	conf *config.Config
+}
+
+func NewClient(conf *config.Config) Client {
+	return &client{
+		conf: conf,
+	}
+}
+
+func (c *client) Start() error {
+	log.Infoln("[Client Gateway] started")
+
+	return nil
+}
