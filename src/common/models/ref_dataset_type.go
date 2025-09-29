0718e6c5547e06e5ae058d5ea0d0d7c93996f615
sev <santisevitz@hotmail.com>
2025-09-29 17:29:50 -0300

    feat: messages are all coherent

--- src/common/models/ref_dataset_type.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type RefDatasetType int
-
-const (
-	MenuItems RefDatasetType = 0
-	Stores    RefDatasetType = 1
-	Users     RefDatasetType = 2
-)
--- src/common/protocol/interface.go
+++
@@ -1,8 +0,0 @@
-package protocol
-
-import "github.com/maxogod/distro-tp/src/common/models/transaction"
-
-type Protocol interface {
-	GetTransactionBatch() (*transaction.TransactionBatch, error)
-	SendTransactionBatch(t *transaction.TransactionBatch) error
-}
