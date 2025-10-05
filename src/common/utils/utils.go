251b6cdf3cf463e9fa35dadc4fe328bc041c1ac7
maxogod <maxi18wars@gmail.com>
2025-10-05 20:55:45 -0300

    chore: clean utils

--- src/common/utils/utils.go
+++ src/common/utils/utils.go
@@ -1,39 +1,9 @@
 package utils
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-
 	"github.com/maxogod/distro-tp/src/common/models/protocol"
 	"google.golang.org/protobuf/proto"
 )
-
-func AppendToCSVFile(path string, payload []byte) error {
-	dir := filepath.Dir(path)
-	if err := os.MkdirAll(dir, 0o755); err != nil {
-		return err
-	}
-
-	f, openErr := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if openErr != nil {
-		return openErr
-	}
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-			fmt.Printf("error closing file: %v\n", err)
-		}
-	}(f)
-
-	if _, err := f.Write(payload); err != nil {
-		return err
-	}
-	if err := f.Sync(); err != nil {
-		return err
-	}
-	return nil
-}
 
 func GetDataEnvelope(msg []byte) (*protocol.DataEnvelope, error) {
 
@@ -46,7 +16,7 @@ func GetDataEnvelope(msg []byte) (*protocol.DataEnvelope, error) {
 	return dataBatch, nil
 }
 
-// This function creates a marshaled DataEnvelope containing the provided data, task type, and client ID.
+// CreateSerializedEnvelope creates a marshaled DataEnvelope containing the provided data, task type, and client ID.
 // This simplifies the need to manually create and marshal DataEnvelope messages each time.
 func CreateSerializedEnvelope(data proto.Message, taskType int32, clientID string) ([]byte, error) {
 	payload, err := proto.Marshal(data)
@@ -61,13 +31,4 @@ func CreateSerializedEnvelope(data proto.Message, taskType int32, clientID strin
 	}
 
 	return proto.Marshal(dataEnvelope)
-}
-
-// UnmarshalPayload unmarshals payload into a proto.Message of type T.
-func UnmarshalPayload[T proto.Message](payload []byte, msg T) (T, error) {
-	err := proto.Unmarshal(payload, msg)
-	if err != nil {
-		return msg, err
-	}
-	return msg, nil
 }
