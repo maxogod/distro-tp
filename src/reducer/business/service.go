445066a27acf870116cb871a06a5b79af722d2f4
sev <santisevitz@hotmail.com>
2025-10-08 19:29:23 -0300

    feat: adding reducer, requires refactor because i just copied the groupby and updated the imports

---
+++ src/reducer/business/service.go
@@ -0,0 +1,8 @@
+package business
+
+type GroupByService struct{}
+
+// TODO: Implement business logic methods
+func NewGroupByService() GroupByService {
+	return GroupByService{}
+}
---
+++ src/reducer/internal/server/server.go
@@ -0,0 +1,84 @@
+package server
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/maxogod/distro-tp/src/common/logger"
+	"github.com/maxogod/distro-tp/src/common/middleware"
+	"github.com/maxogod/distro-tp/src/common/worker"
+	"github.com/maxogod/distro-tp/src/reducer/business"
+	"github.com/maxogod/distro-tp/src/reducer/config"
+	"github.com/maxogod/distro-tp/src/reducer/internal/task_executor"
+)
+
+var log = logger.GetLogger()
+
+type Server struct {
+	messageHandler worker.MessageHandler
+}
+
+func InitServer(conf *config.Config) *Server {
+
+	// initiateOutputs
+	groupByInputQueue := middleware.GetGroupByQueue(conf.Address)
+	reducerOutputQueue := middleware.GetReducerQueue(conf.Address)
+
+	// initiate internal components
+	service := business.NewGroupByService()
+
+	taskExecutor := task_executor.NewGroupByExecutor(
+		service,
+		reducerOutputQueue,
+	)
+
+	taskHandler := worker.NewTaskHandler(taskExecutor)
+
+	messageHandler := worker.NewMessageHandler(
+		taskHandler,
+		[]middleware.MessageMiddleware{groupByInputQueue},
+		nil,
+	)
+
+	return &Server{
+		messageHandler: messageHandler,
+	}
+}
+
+func (s *Server) Run() error {
+	log.Info("Starting Group By server...")
+	s.setupGracefulShutdown()
+
+	// This is a blocking call, it will run until an error occurs or
+	// the Close() method is called via a signal
+	e := s.messageHandler.Start()
+	if e != nil {
+		log.Errorf("Error starting message handler: %v", e)
+		s.Shutdown()
+		return e
+	}
+	return nil
+}
+
+func (s *Server) setupGracefulShutdown() {
+	// This is a graceful non-blocking setup to shut down the process in case
+	sigChannel := make(chan os.Signal, 1)
+	signal.Notify(sigChannel, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		<-sigChannel
+		log.Debugf("Shutdown Signal received, shutting down...")
+		s.Shutdown()
+	}()
+}
+
+func (s *Server) Shutdown() {
+	log.Debug("Shutting down Group By Worker server...")
+	err := s.messageHandler.Close()
+	if err != nil {
+		log.Errorf("Error closing message handler: %v", err)
+	}
+
+	log.Debug("Group By Worker server shut down successfully.")
+}
