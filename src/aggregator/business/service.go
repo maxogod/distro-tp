d17d4438e8da2cb2c49fed948e22cd91cce8c236
sev <santisevitz@hotmail.com>
2025-10-10 09:58:19 -0300

    refactor: optimizing aggregator business logic

--- src/aggregator/business/service.go
+++ src/aggregator/business/service.go
@@ -10,9 +10,6 @@ import (
 
 var log = logger.GetLogger()
 
-// This service acts as a bridge between the business logic and the cache layer.
-// Allowing the aggregator to store or read data from the cache.
-// It requires a cache service to be provided at initialization.
 type AggregatorService struct {
 	cacheService cache.CacheService
 }
@@ -23,40 +20,20 @@ func NewAggregatorService(cacheService cache.CacheService) AggregatorService {
 	}
 }
 
-// This is T1
-func (as *AggregatorService) StoreTransactions(clientID string, transactions []*raw.Transaction) error {
+// ======= GENERIC HELPERS (Private) =======
 
-	protoTransactions := make([]*proto.Message, len(transactions))
-	for i, tx := range transactions {
-		msg := proto.Message(tx)
-		protoTransactions[i] = &msg
+// Generic helper for storing any proto.Message slice
+func storeBatch[T proto.Message](as *AggregatorService, clientID string, data []T) error {
+	protoMessages := make([]*proto.Message, len(data))
+	for i := range data {
+		msg := proto.Message(data[i])
+		protoMessages[i] = &msg
 	}
-
-	return as.cacheService.StoreBatch(clientID, protoTransactions)
-
+	return as.cacheService.StoreBatch(clientID, protoMessages)
 }
 
-// This is T2_1
-func (as *AggregatorService) StoreTotalProfitBySubtotal(clientID string, reducedData []*reduced.TotalProfitBySubtotal) error {
-	return nil
-}
-
-// This is T2_2
-func (as *AggregatorService) StoreTotalSoldByQuantity(clientID string, reducedData []*reduced.TotalSoldByQuantity) error {
-	return nil
-}
-
-// This is T3
-func (as *AggregatorService) StoreTotalPaymentValue(clientID string, reducedData []*reduced.TotalPaymentValue) error {
-	return nil
-}
-
-// This is T4
-func (as *AggregatorService) StoreCountedUserTransactions(clientID string, reducedData []*reduced.CountedUserTransactions) error {
-	return nil
-}
-
-func (as *AggregatorService) GetStoredTransactions(clientID string, amount int32) ([]*raw.Transaction, bool) {
+// Generic helper for getting any proto.Message type
+func getBatch[T proto.Message](as *AggregatorService, clientID string, amount int32) ([]T, bool) {
 	protoMessages, err := as.cacheService.ReadBatch(clientID, amount)
 	if err != nil {
 		log.Errorf("Error reading batch from cache: %v", err)
@@ -67,13 +44,69 @@ func (as *AggregatorService) GetStoredTransactions(clientID string, amount int32
 		return nil, false
 	}
 
-	transactions := make([]*raw.Transaction, len(protoMessages))
+	result := make([]T, len(protoMessages))
 	for i, msg := range protoMessages {
-		transactions[i] = (*msg).(*raw.Transaction)
+		result[i] = (*msg).(T)
 	}
 
-	return transactions, true
+	return result, true
 }
+
+// ======= STORAGE FUNCTIONS =======
+
+// This is T1
+func (as *AggregatorService) StoreTransactions(clientID string, transactions []*raw.Transaction) error {
+	return storeBatch(as, clientID, transactions)
+}
+
+// This is T2_1
+func (as *AggregatorService) StoreTotalProfitBySubtotal(clientID string, reducedData []*reduced.TotalProfitBySubtotal) error {
+	return storeBatch(as, clientID, reducedData)
+}
+
+// This is T2_2
+func (as *AggregatorService) StoreTotalSoldByQuantity(clientID string, reducedData []*reduced.TotalSoldByQuantity) error {
+	return storeBatch(as, clientID, reducedData)
+}
+
+// This is T3
+func (as *AggregatorService) StoreTotalPaymentValue(clientID string, reducedData []*reduced.TotalPaymentValue) error {
+	return storeBatch(as, clientID, reducedData)
+}
+
+// This is T4
+func (as *AggregatorService) StoreCountedUserTransactions(clientID string, reducedData []*reduced.CountedUserTransactions) error {
+	return storeBatch(as, clientID, reducedData)
+}
+
+// ======= RETRIEVAL FUNCTIONS =======
+
+// This is T1
+func (as *AggregatorService) GetStoredTransactions(clientID string, amount int32) ([]*raw.Transaction, bool) {
+	return getBatch[*raw.Transaction](as, clientID, amount)
+}
+
+// This is T2_1
+func (as *AggregatorService) GetStoredTotalProfitBySubtotal(clientID string, amount int32) ([]*reduced.TotalProfitBySubtotal, bool) {
+	return getBatch[*reduced.TotalProfitBySubtotal](as, clientID, amount)
+}
+
+// This is T2_2
+func (as *AggregatorService) GetStoredTotalSoldByQuantity(clientID string, amount int32) ([]*reduced.TotalSoldByQuantity, bool) {
+	return getBatch[*reduced.TotalSoldByQuantity](as, clientID, amount)
+}
+
+// This is T3
+func (as *AggregatorService) GetStoredTotalPaymentValue(clientID string, amount int32) ([]*reduced.TotalPaymentValue, bool) {
+	return getBatch[*reduced.TotalPaymentValue](as, clientID, amount)
+}
+
+// This is T4
+func (as *AggregatorService) GetStoredCountedUserTransactions(clientID string, amount int32) ([]*reduced.CountedUserTransactions, bool) {
+	return getBatch[*reduced.CountedUserTransactions](as, clientID, amount)
+}
+
+// ======= CLOSE =======
 
 func (as *AggregatorService) Close() error {
 	return as.cacheService.Close()
