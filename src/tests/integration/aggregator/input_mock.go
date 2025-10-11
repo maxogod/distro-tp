9375205c04d289de70e428cd14b552880e7e103b
maxogod <maxi18wars@gmail.com>
2025-10-11 11:05:30 -0300

    test: add placeholder test for aggregator

---
+++ src/tests/integration/aggregator/input_mock.go
@@ -0,0 +1,148 @@
+package filter_test
+
+import (
+	"github.com/maxogod/distro-tp/src/common/models/raw"
+	"github.com/maxogod/distro-tp/src/common/models/reduced"
+)
+
+var MockTransactionsBatch = raw.TransactionBatch{
+	Transactions: []*raw.Transaction{
+		{
+			TransactionId: "1", // Good
+			StoreId:       "storeID",
+			UserId:        "userID",
+			FinalAmount:   150.0,
+			CreatedAt:     "2025-07-01 06:01:00",
+		},
+		{
+			TransactionId: "6", // Good
+			StoreId:       "storeID",
+			UserId:        "userID",
+			FinalAmount:   75.0,
+			CreatedAt:     "2025-07-01 06:00:00",
+		},
+		{
+			TransactionId: "7", // Good
+			StoreId:       "storeID",
+			UserId:        "userID",
+			FinalAmount:   100.0,
+			CreatedAt:     "2025-07-01 23:00:00",
+		},
+	}}
+
+var MockTotalProfit = []*reduced.TotalProfitBySubtotal{
+	{
+		ItemId:    "item1",
+		YearMonth: "2025-07",
+		Subtotal:  100.0,
+	},
+	{
+		ItemId:    "item1",
+		YearMonth: "2025-07", // TOP 1 2025-07
+		Subtotal:  100.0,
+	},
+	{
+		ItemId:    "item1",
+		YearMonth: "2024-07",
+		Subtotal:  150.0,
+	},
+	{
+		ItemId:    "item2",
+		YearMonth: "2025-07",
+		Subtotal:  199.0,
+	},
+	{
+		ItemId:    "item3",
+		YearMonth: "2024-07", // TOP 1 2024-07
+		Subtotal:  199.0,
+	},
+}
+
+var MockTotalSales = []*reduced.TotalSoldByQuantity{
+	{
+		ItemId:    "item1",
+		YearMonth: "2025-07",
+		Quantity:  100.0,
+	},
+	{
+		ItemId:    "item1",
+		YearMonth: "2025-07", // TOP 1 2025-07
+		Quantity:  100.0,
+	},
+	{
+		ItemId:    "item1",
+		YearMonth: "2024-07",
+		Quantity:  150.0,
+	},
+	{
+		ItemId:    "item2",
+		YearMonth: "2025-07",
+		Quantity:  199.0,
+	},
+	{
+		ItemId:    "item3",
+		YearMonth: "2024-07", // TOP 1 2024-07
+		Quantity:  199.0,
+	},
+}
+
+var MockTPV = []*reduced.TotalPaymentValue{
+	{
+		StoreId:     "1",
+		Semester:    "2025-H2",
+		FinalAmount: 100.0,
+	},
+	{
+		StoreId:     "1",
+		Semester:    "2025-H2", // TOP 1 2025-H2
+		FinalAmount: 100.0,
+	},
+	{
+		StoreId:     "1",
+		Semester:    "2024-H2",
+		FinalAmount: 100.0,
+	},
+	{
+		StoreId:     "2",
+		Semester:    "2025-H2",
+		FinalAmount: 199.0,
+	},
+	{
+		StoreId:     "3",
+		Semester:    "2024-H2", // TOP 1 2024-H2
+		FinalAmount: 199.0,
+	},
+}
+
+var MockUsers = []*reduced.CountedUserTransactions{
+	{
+		StoreId:             "1",
+		UserId:              "user1",
+		Birthdate:           "2000-01-01",
+		TransactionQuantity: 100,
+	},
+	{
+		StoreId:             "1",
+		UserId:              "user1",
+		Birthdate:           "2000-01-01", // TOP 1 user1@store1
+		TransactionQuantity: 100,
+	},
+	{
+		StoreId:             "2",
+		UserId:              "user1",
+		Birthdate:           "2000-02-02",
+		TransactionQuantity: 100,
+	},
+	{
+		StoreId:             "1",
+		UserId:              "user2",
+		Birthdate:           "2000-02-02",
+		TransactionQuantity: 199,
+	},
+	{
+		StoreId:             "2",
+		UserId:              "user3",
+		Birthdate:           "2000-02-02", // TOP 1 user3@store2
+		TransactionQuantity: 199,
+	},
+}
---
+++ src/tests/integration/aggregator/integration_test.go
@@ -0,0 +1,94 @@
+package filter_test
+
+import (
+	"testing"
+
+	"github.com/maxogod/distro-tp/src/common/middleware"
+	"github.com/maxogod/distro-tp/src/filter/mock"
+)
+
+var url = "amqp://guest:guest@localhost:5672/"
+
+func TestMain(m *testing.M) {
+	go mock.StartFilterMock("./config_test.yaml")
+	m.Run()
+}
+
+// TestSequentialRun runs tests in sequence to
+// avoid consuming conflicts on the same queues.
+func TestSequentialRun(t *testing.T) {
+	tests := []func(t *testing.T){
+		t1AggregateMock,
+		t2_1AggregateMock,
+		t2_2AggregateMock,
+		t3AggregateMock,
+		t4AggregateMock,
+	}
+
+	// Run each test one by one
+	for _, test := range tests {
+		test(t)
+	}
+
+	aggregatorOutputQueue := middleware.GetAggregatorQueue(url)
+	aggregatorOutputQueue.Delete()
+}
+
+func t1AggregateMock(t *testing.T) {
+	aggregatorOutputQueue := middleware.GetAggregatorQueue(url)
+	defer aggregatorOutputQueue.StopConsuming()
+	defer aggregatorOutputQueue.Close()
+
+	// 	serializedTransactions, _ := proto.Marshal(&MockTransactionsBatch)
+	// 	dataEnvelope := protocol.DataEnvelope{
+	// 		ClientId: "test-client1",
+	// 		TaskType: int32(enum.T1),
+	// 		Payload:  serializedTransactions,
+	// 	}
+	// 	serializedDataEnvelope, _ := proto.Marshal(&dataEnvelope)
+
+	// 	filterQueue.Send(serializedDataEnvelope)
+
+	// 	res := &raw.TransactionBatch{}
+	// 	done := make(chan bool, 1)
+	// 	e := aggregatorOutputQueue.StartConsuming(func(consumeChannel middleware.ConsumeChannel, d chan error) {
+	// 		for msg := range consumeChannel {
+	// 			msg.Ack(false)
+
+	// 			dataBatch, _ := utils.GetDataEnvelope(msg.Body)
+
+	// 			TransactionData := &raw.TransactionBatch{}
+	// 			err := proto.Unmarshal(dataBatch.Payload, TransactionData)
+	// 			assert.Nil(t, err)
+
+	//			res.Transactions = TransactionData.Transactions
+	//			break
+	//		}
+	//		done <- true
+	//	})
+	//
+	// select {
+	// case <-done:
+	// case <-time.After(5 * time.Second):
+	//
+	//		t.Error("Test timed out waiting for results")
+	//	}
+	//
+	// assert.Equal(t, 0, int(e))
+	// assert.Equal(t, 3, len(res.Transactions), "Expected 2 transactions after filtering")
+	// assert.Equal(t, "1", res.Transactions[0].TransactionId)
+	// assert.Equal(t, "6", res.Transactions[1].TransactionId)
+	// assert.Equal(t, "7", res.Transactions[2].TransactionId)
+}
+
+func t2_1AggregateMock(t *testing.T) {
+}
+
+func t2_2AggregateMock(t *testing.T) {
+}
+
+func t3AggregateMock(t *testing.T) {
+}
+
+func t4AggregateMock(t *testing.T) {
+}
