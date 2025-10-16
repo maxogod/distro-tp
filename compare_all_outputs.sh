5bdf1aa01561742a6404074940ddc26759a2e0d9
maxogod <maxi18wars@gmail.com>
2025-10-16 02:38:09 -0300

    tests: compare all outputs

---
+++ compare_all_outputs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OUTPUT_FILES=(.output*/**)
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 <expected_folder>"
+    exit 1
+elif [ ${#OUTPUT_FILES[@]} -eq 0 ]; then
+    echo "No output files found."
+    exit 1
+fi
+
+EXPECTED_FOLDER="$1"
+
+for output_file in "${OUTPUT_FILES[@]}"; do
+    filename=$(basename "$output_file")
+    
+    expected_file="$EXPECTED_FOLDER/$filename"
+    
+    if [ ! -f "$expected_file" ]; then
+        echo "Expected file not found for $filename"
+        continue
+    fi
+
+    python3 ./compare_output.py "$output_file" "$expected_file"
+done
+
