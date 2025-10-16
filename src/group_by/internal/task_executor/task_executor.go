e50458b37587b8408a227d1dea006a25971692b9
sev <santisevitz@hotmail.com>
2025-10-16 00:10:01 -0300

    slightly reduce group by logic

--- src/group_by/internal/task_executor/task_executor.go
+++ src/group_by/internal/task_executor/task_executor.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/maxogod/distro-tp/src/common/logger"
 	"github.com/maxogod/distro-tp/src/common/middleware"
 	"github.com/maxogod/distro-tp/src/common/models/enum"
 	"github.com/maxogod/distro-tp/src/common/models/raw"
@@ -12,24 +13,31 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-const FLUSH_TIMEOUT = 3 * time.Second
-const MAX_ACCUMULATE = 1000
+var log = logger.GetLogger()
 
-type groupAcumulator struct {
-	data  []proto.Message
-	timer *time.Timer
+const FLUSH_TIMEOUT = 500 * time.Millisecond
+
+type groupData struct {
+	data   []proto.Message
+	isDone bool
+}
+
+type groupAccumulator struct {
+	dataChan chan groupData
+	timer    *time.Timer
 }
 
 type GroupExecutor struct {
 	service          business.GroupService
 	reducerQueue     middleware.MessageMiddleware
-	groupAccumulator map[string]groupAcumulator
+	groupAccumulator map[string]groupAccumulator
 }
 
-func NewGroupExecutor(filterService business.GroupService, reducerQueue middleware.MessageMiddleware) worker.TaskExecutor {
+func NewGroupExecutor(groupService business.GroupService, reducerQueue middleware.MessageMiddleware) worker.TaskExecutor {
 	return &GroupExecutor{
-		service:      filterService,
-		reducerQueue: reducerQueue,
+		service:          groupService,
+		reducerQueue:     reducerQueue,
+		groupAccumulator: make(map[string]groupAccumulator),
 	}
 }
 
@@ -39,16 +47,16 @@ func (fe *GroupExecutor) HandleTask2(payload []byte, clientID string) error {
 	if err != nil {
 		return err
 	}
-
-	flushFn := func(id string) error {
-		flushed := fe.FlushGroups(id)
+	flushFn := func(id string, flushed []proto.Message) error {
+		if len(flushed) == 0 {
+			return nil
+		}
 		transactionItems := make([]*raw.TransactionItem, len(flushed))
 		for i, msg := range flushed {
 			transactionItems[i] = msg.(*raw.TransactionItem)
 		}
 		// === Business logic ===
 		groupedData := fe.service.GroupItemsByYearMonthAndItem(transactionItems)
-
 		// This output is sent to both T2.1 and T2.2
 		// So we iterate over the map and send each grouped data to both queues
 		// This will increase the traffic twice as much as any other task
@@ -64,11 +72,12 @@ func (fe *GroupExecutor) HandleTask2(payload []byte, clientID string) error {
 		}
 		return nil
 	}
+	fe.initGroupFlusher(clientID, flushFn)
 	protoData := make([]proto.Message, len(transactionBatch.GetTransactionItems()))
 	for i, item := range transactionBatch.GetTransactionItems() {
 		protoData[i] = item
 	}
-	fe.AccumulateGroups(protoData, clientID, flushFn)
+	fe.AccumulateGroups(protoData, clientID)
 	return nil
 }
 
@@ -79,15 +88,16 @@ func (fe *GroupExecutor) HandleTask3(payload []byte, clientID string) error {
 		return err
 	}
 
-	flushFn := func(id string) error {
-		flushed := fe.FlushGroups(id)
+	flushFn := func(id string, flushed []proto.Message) error {
+		if len(flushed) == 0 {
+			return nil
+		}
 		transactions := make([]*raw.Transaction, len(flushed))
 		for i, msg := range flushed {
 			transactions[i] = msg.(*raw.Transaction)
 		}
 		// === Business logic ===
 		groupedData := fe.service.GroupTransactionsByStoreAndSemester(transactions)
-
 		for _, group := range groupedData {
 			err := worker.SendDataToMiddleware(group, enum.T3, clientID, fe.reducerQueue)
 			if err != nil {
@@ -96,11 +106,14 @@ func (fe *GroupExecutor) HandleTask3(payload []byte, clientID string) error {
 		}
 		return nil
 	}
+
+	fe.initGroupFlusher(clientID, flushFn)
+
 	protoData := make([]proto.Message, len(transactionBatch.GetTransactions()))
 	for i, item := range transactionBatch.GetTransactions() {
 		protoData[i] = item
 	}
-	fe.AccumulateGroups(protoData, clientID, flushFn)
+	fe.AccumulateGroups(protoData, clientID)
 	return nil
 }
 
@@ -111,14 +124,17 @@ func (fe *GroupExecutor) HandleTask4(payload []byte, clientID string) error {
 		return err
 	}
 
-	flushFn := func(id string) error {
-		flushed := fe.FlushGroups(id)
+	flushFn := func(id string, flushed []proto.Message) error {
+		if len(flushed) == 0 {
+			return nil
+		}
 		transactions := make([]*raw.Transaction, len(flushed))
 		for i, msg := range flushed {
 			transactions[i] = msg.(*raw.Transaction)
 		}
+
 		// === Business logic ===
-		groupedData := fe.service.GroupTransactionsByStoreAndUser(transactionBatch.GetTransactions())
+		groupedData := fe.service.GroupTransactionsByStoreAndUser(transactions)
 
 		for _, group := range groupedData {
 			err := worker.SendDataToMiddleware(group, enum.T4, clientID, fe.reducerQueue)
@@ -128,11 +144,14 @@ func (fe *GroupExecutor) HandleTask4(payload []byte, clientID string) error {
 		}
 		return nil
 	}
+
+	fe.initGroupFlusher(clientID, flushFn)
+
 	protoData := make([]proto.Message, len(transactionBatch.GetTransactions()))
 	for i, item := range transactionBatch.GetTransactions() {
 		protoData[i] = item
 	}
-	fe.AccumulateGroups(protoData, clientID, flushFn)
+	fe.AccumulateGroups(protoData, clientID)
 	return nil
 }
 
@@ -161,53 +180,48 @@ func (fe *GroupExecutor) HandleFinishClient(clientID string) error {
 	panic("The group by worker does not require client finishing handling")
 }
 
-func (fe *GroupExecutor) AccumulateGroups(data []proto.Message, clientID string, flushFn func(string) error) error {
-	if fe.groupAccumulator == nil {
-		fe.groupAccumulator = make(map[string]groupAcumulator)
-	}
-
+func (fe *GroupExecutor) AccumulateGroups(data []proto.Message, clientID string) error {
 	// Get or create the accumulator for this client
 	acc, exists := fe.groupAccumulator[clientID]
+
 	if !exists {
-		acc = groupAcumulator{
-			data:  []proto.Message{},
-			timer: nil,
-		}
+		return fmt.Errorf("no accumulator found for client %s", clientID)
 	}
 
-	// Append new data
-	acc.data = append(acc.data, data...)
-
-	// If a timer exists, stop it before starting a new one
-	if acc.timer != nil {
-		acc.timer.Stop()
-	}
-
-	if len(acc.data) >= MAX_ACCUMULATE {
-		e := flushFn(clientID)
-		if e != nil {
-			return e
-		}
-		return nil
-	}
-
-	// Start a new timer
-	acc.timer = time.AfterFunc(FLUSH_TIMEOUT, func() {
-		e := flushFn(clientID)
-		if e != nil {
-			fmt.Printf("Error flushing groups for client %s: %v\n", clientID, e)
-		}
-	})
-
-	fe.groupAccumulator[clientID] = acc
+	acc.dataChan <- groupData{data: data, isDone: false}
 	return nil
 }
 
-func (fe *GroupExecutor) FlushGroups(clientID string) []proto.Message {
-	acc, exists := fe.groupAccumulator[clientID]
-	if !exists {
-		return nil
+func (fe *GroupExecutor) initGroupFlusher(clientID string, flushFn func(string, []proto.Message) error) {
+
+	// This method should actually be called once, so th handle it we use this
+	_, exists := fe.groupAccumulator[clientID]
+	if exists {
+		return
 	}
-	delete(fe.groupAccumulator, clientID)
-	return acc.data
+
+	acc := groupAccumulator{
+		dataChan: make(chan groupData),
+		timer:    nil,
+	}
+	acc.timer = time.AfterFunc(FLUSH_TIMEOUT, func() {
+		acc.dataChan <- groupData{data: nil, isDone: true}
+		acc.timer.Reset(FLUSH_TIMEOUT)
+	})
+	fe.groupAccumulator[clientID] = acc
+
+	go func() {
+		flushData := []proto.Message{}
+		for {
+			grp := <-acc.dataChan
+			if grp.isDone {
+				err := flushFn(clientID, flushData)
+				if err != nil {
+					log.Debugf("failed to flush data for client %s: %v", clientID, err)
+				}
+				flushData = []proto.Message{}
+			}
+			flushData = append(flushData, grp.data...)
+		}
+	}()
 }
