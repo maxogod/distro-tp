9f0c9fc857b31fa8a7ddb8e88eac7a28c2e1b34c
maxogod <maxi18wars@gmail.com>
2025-10-16 03:29:42 -0300

    docs: add reminder to run rabbit in integration

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -2,6 +2,9 @@
 
 TEST_PATH="./src/tests/integration/"
 
+echo "Remember these tests need a rabbit instance up!"
+echo "Run: docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:4-management"
+
 for test_file in $(ls $TEST_PATH); do
   echo "Running tests in $test_file"
   go test "$TEST_PATH$test_file/..." -count=1
