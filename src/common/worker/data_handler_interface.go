987d6a696699b71607ff55f526693696b3673150
Federico Genaro <fedegen14@gmail.com>
2025-10-18 10:11:43 -0300

    refactor: change DataHandler interface to receive data envelope and ack handler

--- src/common/worker/data_handler_interface.go
+++ src/common/worker/data_handler_interface.go
@@ -5,7 +5,7 @@ import "github.com/maxogod/distro-tp/src/common/models/protocol"
 // This interface is required for workers to use when attempting to handle data,
 // This must go in tandem with the MessageHandler struct
 type DataHandler interface {
-	HandleData(dataEnvelope *protocol.DataEnvelope) error
-	HandleFinishClient(clientID string) error
+	HandleData(dataEnvelope *protocol.DataEnvelope, ackHandler func(bool) error) error
+	HandleFinishClient(dataEnvelope *protocol.DataEnvelope, ackHandler func(bool) error) error
 	Close() error
 }
