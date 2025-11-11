9c89af30f310296e576d5b84040ad00aa7e0d835
sev <santisevitz@hotmail.com>
2025-11-10 21:28:06 -0300

    update tests to not show errors

--- src/tests/integration/reducer/output_mock.go
+++ src/tests/integration/reducer/output_mock.go
@@ -10,18 +10,6 @@ var ReducedTransactionMock1 = reduced.TotalPaymentValue{
 	FinalAmount: 200.0,
 }
 
-var ReducedTransactionMock2_1 = reduced.TotalProfitBySubtotal{
-	ItemId:    "item1",
-	YearMonth: "2025-07",
-	Subtotal:  400,
-}
-
-var ReducedTransactionMock2_2 = reduced.TotalSoldByQuantity{
-	ItemId:    "item1",
-	YearMonth: "2025-07",
-	Quantity:  4,
-}
-
 var ReducedTransactionMock4 = reduced.CountedUserTransactions{
 	StoreId:             "store3",
 	UserId:              "user1",
