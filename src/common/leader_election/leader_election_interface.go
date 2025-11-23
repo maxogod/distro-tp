30d5839b8d95213e9eec6b610722cf60e8c24e80
maxogod <maxi18wars@gmail.com>
2025-11-23 17:45:40 -0300

    docs

--- src/common/leader_election/leader_election_interface.go
+++ src/common/leader_election/leader_election_interface.go
@@ -4,9 +4,16 @@ import "github.com/maxogod/distro-tp/src/common/models/protocol"
 
 // UpdateCallbacks is a wrapper for nullable callbacks
 type UpdateCallbacks struct {
+
+	// ResetUpdates removes all old updates
 	ResetUpdates func()
-	GetUpdates   func(receivingCh chan *protocol.DataEnvelope)
-	SendUpdates  func(sendingCh chan *protocol.DataEnvelope, done chan bool)
+
+	// GetUpdates saves data coming out of the channel
+	GetUpdates func(receivingCh chan *protocol.DataEnvelope)
+
+	// SendUpdates takes as reference all the data received up to this function call
+	// and puts all the data in the channel and closes it, or until DONE is received from the doneCh
+	SendUpdates func(sendingCh chan *protocol.DataEnvelope, done chan bool)
 }
 
 // LeaderElection interface defines the methods required for implementing
