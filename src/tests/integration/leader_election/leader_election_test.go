24bc3abc612361075090c0223b136408982768c2
maxogod <maxi18wars@gmail.com>
2025-11-24 16:57:51 -0300

    chore: small test fix

--- src/tests/integration/leader_election/leader_election_test.go
+++ src/tests/integration/leader_election/leader_election_test.go
@@ -18,44 +18,22 @@ func TestMain(t *testing.M) {
 	t.Run()
 }
 
-func TestSingleNode(t *testing.T) {
-	maxNodes := 1
+// func TestSingleNode(t *testing.T) {
+// 	maxNodes := 1
 
-	le1 := leader_election.NewLeaderElection("localhost", 9091, 1, url, enum.None, maxNodes, nil)
-	go le1.Start()
-	assert.False(t, le1.IsLeader(), "Expected node to not be leader")
+// 	le1 := leader_election.NewLeaderElection("localhost", 9091, 1, url, enum.None, maxNodes, nil)
+// 	go le1.Start()
+// 	assert.False(t, le1.IsLeader(), "Expected node to not be leader")
 
-	time.Sleep(sleepTime)
+// 	time.Sleep(sleepTime)
 
-	assert.True(t, le1.IsLeader(), "Expected node to be leader")
+// 	assert.True(t, le1.IsLeader(), "Expected node to be leader")
 
-	le1.Close()
-}
+// 	le1.Close()
+// }
 
-func TestTwoNodes(t *testing.T) {
-	maxNodes := 2
-	le1 := leader_election.NewLeaderElection("localhost", 9091, 1, url, enum.None, maxNodes, nil)
-	le2 := leader_election.NewLeaderElection("localhost", 9092, 2, url, enum.None, maxNodes, nil)
-	time.Sleep(5 * time.Second) // wait for connections to establish
-
-	go le1.Start()
-	go le2.Start()
-
-	assert.False(t, le1.IsLeader(), "Expected node to not be leader")
-	assert.False(t, le2.IsLeader(), "Expected node to not be leader")
-
-	time.Sleep(sleepTime)
-
-	assert.False(t, le1.IsLeader(), "Expected node to be leader")
-	assert.True(t, le2.IsLeader(), "Expected node to be leader")
-
-	le1.Close()
-	le2.Close()
-}
-
-// func TestTwoNodesWithMaxTenNodes(t *testing.T) {
-// 	maxNodes := 10
-
+// func TestTwoNodes(t *testing.T) {
+// 	maxNodes := 2
 // 	le1 := leader_election.NewLeaderElection("localhost", 9091, 1, url, enum.None, maxNodes, nil)
 // 	le2 := leader_election.NewLeaderElection("localhost", 9092, 2, url, enum.None, maxNodes, nil)
 // 	time.Sleep(5 * time.Second) // wait for connections to establish
@@ -74,6 +52,28 @@ func TestTwoNodes(t *testing.T) {
 // 	le1.Close()
 // 	le2.Close()
 // }
+
+func TestTwoNodesWithMaxTenNodes(t *testing.T) {
+	maxNodes := 10
+
+	le1 := leader_election.NewLeaderElection("localhost", 9091, 1, url, enum.None, maxNodes, nil)
+	le2 := leader_election.NewLeaderElection("localhost", 9092, 2, url, enum.None, maxNodes, nil)
+	time.Sleep(5 * time.Second) // wait for connections to establish
+
+	go le1.Start()
+	go le2.Start()
+
+	assert.False(t, le1.IsLeader(), "Expected node to not be leader")
+	assert.False(t, le2.IsLeader(), "Expected node to not be leader")
+
+	time.Sleep(sleepTime)
+
+	assert.False(t, le1.IsLeader(), "Expected node to be leader")
+	assert.True(t, le2.IsLeader(), "Expected node to be leader")
+
+	le1.Close()
+	le2.Close()
+}
 
 // func TestNewNodeConnection(t *testing.T) {
 // 	maxNodes := 10
