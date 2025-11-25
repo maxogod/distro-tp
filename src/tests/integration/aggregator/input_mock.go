8e553f8a0979a8a53eec7589ef4f8dfdaeae1d3a
Federico Genaro <fedegen14@gmail.com>
2025-11-25 17:03:58 -0300

    refactor: remove t1Aggregate test

--- src/tests/integration/aggregator/input_mock.go
+++ src/tests/integration/aggregator/input_mock.go
@@ -1,34 +1,8 @@
 package aggregator_test
 
 import (
-	"github.com/maxogod/distro-tp/src/common/models/raw"
 	"github.com/maxogod/distro-tp/src/common/models/reduced"
 )
-
-var MockTransactionsBatch = raw.TransactionBatch{
-	Transactions: []*raw.Transaction{
-		{
-			TransactionId: "1", // Good
-			StoreId:       "storeID",
-			UserId:        "userID",
-			FinalAmount:   150.0,
-			CreatedAt:     "2025-07-01 06:01:00",
-		},
-		{
-			TransactionId: "6", // Good
-			StoreId:       "storeID",
-			UserId:        "userID",
-			FinalAmount:   75.0,
-			CreatedAt:     "2025-07-01 06:00:00",
-		},
-		{
-			TransactionId: "7", // Good
-			StoreId:       "storeID",
-			UserId:        "userID",
-			FinalAmount:   100.0,
-			CreatedAt:     "2025-07-01 23:00:00",
-		},
-	}}
 
 var MockTPV = reduced.TotalPaymentValueBatch{
 	TotalPaymentValues: []*reduced.TotalPaymentValue{
--- src/tests/integration/aggregator/integration_test.go
+++ src/tests/integration/aggregator/integration_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/maxogod/distro-tp/src/common/middleware"
 	"github.com/maxogod/distro-tp/src/common/models/enum"
 	"github.com/maxogod/distro-tp/src/common/models/protocol"
-	"github.com/maxogod/distro-tp/src/common/models/raw"
 	"github.com/maxogod/distro-tp/src/common/models/reduced"
 	"github.com/maxogod/distro-tp/src/common/utils"
 	"github.com/stretchr/testify/assert"
@@ -28,7 +27,6 @@ func TestMain(m *testing.M) {
 // avoid consuming conflicts on the same queues.
 func TestSequentialRun(t *testing.T) {
 	tests := []func(t *testing.T){
-		t1AggregateMock,
 		t3AggregateMock,
 		t4AggregateMock,
 	}
@@ -45,79 +43,6 @@ func TestSequentialRun(t *testing.T) {
 	aggregatorInputQueue.Delete()
 	joinerQueue.Delete()
 	processedDataQueue.Delete()
-}
-
-func t1AggregateMock(t *testing.T) {
-	aggregatorInputQueue := middleware.GetAggregatorQueue(url)
-	finishExchange := middleware.GetFinishExchange(url, []string{string(enum.AggregatorWorker)})
-	clientID := "test-client-1"
-	processedDataQueue := middleware.GetProcessedDataExchange(url, clientID)
-
-	var transactions []*raw.Transaction
-	done := make(chan bool, 1)
-	e := processedDataQueue.StartConsuming(func(consumeChannel middleware.ConsumeChannel, d chan error) {
-		for msg := range consumeChannel {
-			msg.Ack(false)
-			dataBatch, _ := utils.GetDataEnvelope(msg.Body)
-
-			t.Logf("Received message from aggregator: %v", dataBatch)
-
-			if dataBatch.IsDone {
-				t.Logf("Received done message from aggregator. Exiting consumer.")
-				break
-			}
-
-			transactionBatch := &raw.TransactionBatch{}
-			err := proto.Unmarshal(dataBatch.Payload, transactionBatch)
-			assert.Nil(t, err)
-
-			transactions = append(transactions, transactionBatch.Transactions...)
-			break
-		}
-		done <- true
-	})
-
-	// Send T1 data to aggregator
-	serializedTransactions, _ := proto.Marshal(&MockTransactionsBatch)
-	dataEnvelope := protocol.DataEnvelope{
-		ClientId: clientID,
-		TaskType: int32(enum.T1),
-		Payload:  serializedTransactions,
-	}
-	serializedDataEnvelope, _ := proto.Marshal(&dataEnvelope)
-
-	aggregatorInputQueue.Send(serializedDataEnvelope)
-
-	// Send done message to aggregator
-	doneMessage := &protocol.DataEnvelope{
-		ClientId: clientID,
-		IsDone:   true,
-		TaskType: int32(enum.T1),
-	}
-	doneBytes, _ := proto.Marshal(doneMessage)
-	time.Sleep(3 * time.Second)
-	err := finishExchange.Send(doneBytes)
-	assert.Equal(t, err, middleware.MessageMiddlewareSuccess)
-
-	select {
-	case <-done:
-	case <-time.After(5 * time.Second):
-		t.Error("Test timed out waiting for results")
-	}
-
-	assert.Equal(t, 0, int(e))
-
-	assert.Len(t, transactions, 3, "Expected 3 transactions after aggregating")
-	if len(transactions) >= 3 {
-		assert.Equal(t, MockTransactionsBatch.GetTransactions()[0].TransactionId, transactions[0].TransactionId)
-		assert.Equal(t, MockTransactionsBatch.GetTransactions()[1].TransactionId, transactions[1].TransactionId)
-		assert.Equal(t, MockTransactionsBatch.GetTransactions()[2].TransactionId, transactions[2].TransactionId)
-	}
-
-	processedDataQueue.StopConsuming()
-	processedDataQueue.Close()
-	aggregatorInputQueue.Close()
-	finishExchange.Close()
 }
 
 func t3AggregateMock(t *testing.T) {
