7d2589bfc90d8373f7b71b4d0c3d037418e5eea7
Federico Genaro <fedegen14@gmail.com>
2025-11-25 18:19:04 -0300

    test: pass TestNewNodeLeaderDiesMidUpdates in leader election

--- src/tests/integration/leader_election/leader_election_test.go
+++ src/tests/integration/leader_election/leader_election_test.go
@@ -233,75 +233,6 @@ func TestNewNodeUpdates(t *testing.T) {
 
 	time.Sleep(sleepTime)
 
-//func TestNewNodeLeaderDiesMidUpdates(t *testing.T) {
-//	maxNodes := 3
-//	var sent atomic.Bool
-//	var resetted atomic.Bool
-//	var updated atomic.Bool
-//
-//	// updatesCallbacks := &leader_election.UpdateCallbacks{
-//	// 	SendUpdates: func(msgs chan *protocol.DataEnvelope, _ chan bool) {
-//	// 		for i := range 3 {
-//	// 			data := &protocol.DataEnvelope{
-//	// 				SequenceNumber: int32(i),
-//	// 			}
-//	// 			msgs <- data
-//	// 			time.Sleep(1 * time.Second)
-//	// 		}
-//	// 		data := &protocol.DataEnvelope{
-//	// 			IsDone: true,
-//	// 		}
-//	// 		msgs <- data
-//	// 		close(msgs)
-//	// 		sent.Store(true)
-//	// 	},
-//	// 	GetUpdates: func(msgs chan *protocol.DataEnvelope) {
-//	// 		i := 0
-//	// 		for msg := range msgs {
-//	// 			assert.Equal(t, msg.GetSequenceNumber(), int32(i), "Expected correct seq num")
-//	// 			i++
-//	// 		}
-//	// 		updated.Store(true)
-//	// 	},
-//	// 	ResetUpdates: func() {
-//	// 		resetted.Store(true)
-//	// 	},
-//	// }
-//
-//	le1 := leader_election.NewLeaderElection("localhost", 9091, 1, url, enum.None, maxNodes, updatesCallbacks)
-//	le2 := leader_election.NewLeaderElection("localhost", 9092, 2, url, enum.None, maxNodes, updatesCallbacks)
-//
-//	go le1.Start()
-//	go le2.Start()
-//
-//	assert.False(t, le1.IsLeader(), "Expected node to not be leader")
-//	assert.False(t, le2.IsLeader(), "Expected node to not be leader")
-//
-//	time.Sleep(sleepTime)
-//
-//	assert.False(t, le1.IsLeader(), "Expected node to be leader")
-//	assert.True(t, le2.IsLeader(), "Expected node to be leader")
-//
-//	// Connect a new node with higher ID
-//	le3 := leader_election.NewLeaderElection("localhost", 9093, 3, url, enum.None, maxNodes, updatesCallbacks)
-//	go le3.Start()
-//
-//	time.Sleep(1 * time.Second)
-//
-//	le2.Close() // Leader dies mid updates
-//
-//	time.Sleep(3 * sleepTime)
-//
-//	assert.False(t, le1.IsLeader(), "Expected node to be leader")
-//	assert.True(t, le3.IsLeader(), "Expected new node to not be leader")
-//
-//	assert.True(t, sent.Load(), "Expected updates to be sent")
-//	assert.True(t, updated.Load(), "Expected updates to be received")
-//	assert.True(t, resetted.Load(), "Expected updates to be resetted")
-//
-//	le1.Close()
-//	le3.Close()
-//}
 	assert.False(t, le1.IsLeader(), "Expected node to be leader")
 	assert.True(t, le2.IsLeader(), "Expected node to be leader")
 
@@ -323,3 +254,49 @@ func TestNewNodeUpdates(t *testing.T) {
 	le3.Close()
 }
 
+func TestNewNodeLeaderDiesMidUpdates(t *testing.T) {
+	maxNodes := 3
+	sent := &atomic.Bool{}
+	resetted := &atomic.Bool{}
+	updated := &atomic.Bool{}
+	updatesCallbacks := &testUpdatesCallbacks{
+		sent:     sent,
+		updated:  updated,
+		resetted: resetted,
+		t:        t,
+	}
+
+	le1 := leader_election.NewLeaderElection("localhost", 9091, 1, url, enum.None, maxNodes, updatesCallbacks)
+	le2 := leader_election.NewLeaderElection("localhost", 9092, 2, url, enum.None, maxNodes, updatesCallbacks)
+
+	go le1.Start()
+	go le2.Start()
+
+	assert.False(t, le1.IsLeader(), "Expected node to not be leader")
+	assert.False(t, le2.IsLeader(), "Expected node to not be leader")
+
+	time.Sleep(sleepTime)
+
+	assert.False(t, le1.IsLeader(), "Expected node to be leader")
+	assert.True(t, le2.IsLeader(), "Expected node to be leader")
+
+	// Connect a new node with higher ID
+	le3 := leader_election.NewLeaderElection("localhost", 9093, 3, url, enum.None, maxNodes, updatesCallbacks)
+	go le3.Start()
+
+	time.Sleep(1 * time.Second)
+
+	le2.Close() // Leader dies mid updates
+
+	time.Sleep(3 * sleepTime)
+
+	assert.False(t, le1.IsLeader(), "Expected node to be leader")
+	assert.True(t, le3.IsLeader(), "Expected new node to not be leader")
+
+	assert.True(t, sent.Load(), "Expected updates to be sent")
+	assert.True(t, updated.Load(), "Expected updates to be received")
+	assert.True(t, resetted.Load(), "Expected updates to be resetted")
+
+	le1.Close()
+	le3.Close()
+}
