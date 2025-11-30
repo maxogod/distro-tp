b21287d305dc121d7203f42f7253f88c43ec4df6
Federico Genaro <fedegen14@gmail.com>
2025-11-30 02:46:01 -0300

    refactor: client connects to one gateway again

--- src/client/internal/client/client.go
+++ src/client/internal/client/client.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 
 	"github.com/google/uuid"
@@ -24,15 +23,10 @@ type client struct {
 
 func NewClient(conf *config.Config) (Client, error) {
 	clientID := uuid.New().String()
-	connections, err := connectToGateways(conf, clientID)
+	// TODO: Cambiar para conectarnos a un gateway random
+	conn, err := connectToGateway(conf)
 	if err != nil {
 		logger.Logger.Errorf("could not connect to gateways: %v", err)
-		return nil, err
-	}
-
-	conn, err := awaitLeaderGateway(connections)
-	if err != nil {
-		logger.Logger.Errorf("could not determine leader gateway: %v", err)
 		return nil, err
 	}
 
@@ -71,7 +65,6 @@ func (c *client) Start(task string) error {
 
 func (c *client) Shutdown() {
 	c.running = false
-	// TODO: Cerrar todas las conexiones
 	c.conn.Close()
 	c.tastExecutor.Close()
 	logger.Logger.Infof("action: shutdown | result: success")
@@ -97,12 +90,11 @@ func (c *client) handleTaskError(err error) error {
 	return err
 }
 
-func connectToGateways(conf *config.Config, clientID string) (map[int]network.ConnectionInterface, error) {
+func connectToGateway(conf *config.Config) (network.ConnectionInterface, error) {
 	if conf.MaxNodes <= 0 {
 		return nil, fmt.Errorf("gateway count must be positive")
 	}
 
-	connections := make(map[int]network.ConnectionInterface, conf.MaxNodes)
 	for i := 1; i <= conf.MaxNodes; i++ {
 		conn := network.NewConnection()
 		serverAddr := fmt.Sprintf("%s%d:%d", conf.ServerHost, i, conf.ServerPort)
@@ -111,59 +103,8 @@ func connectToGateways(conf *config.Config, clientID string) (map[int]network.Co
 			continue
 		}
 
-		connections[i] = conn
-
-		// TODO: Mandar un DataEnvelope en vez del ID directamente
-		if err := conn.SendData([]byte(clientID)); err != nil {
-			return nil, fmt.Errorf("failed to send client ID: %w", err)
-		}
+		return conn, nil
 	}
 
-	if len(connections) == 0 {
-		return nil, fmt.Errorf("could not connect to any gateway")
-	}
-
-	return connections, nil
-}
-
-func awaitLeaderGateway(conns map[int]network.ConnectionInterface) (network.ConnectionInterface, error) {
-	respCh := make(chan int, len(conns))
-
-	for idx, conn := range conns {
-		go func(index int, connection network.ConnectionInterface) {
-			idBytes, err := connection.ReceiveData()
-			if err != nil {
-				logger.Logger.Errorf("connection with gateway%d closed", index)
-				return
-			}
-
-			id := string(idBytes)
-			logger.Logger.Infof("ReadyForData received from gateway%s", id)
-
-			idInt, err := strconv.Atoi(id)
-			if err != nil {
-				logger.Logger.Errorf("invalid gateway id received from gateway%d: %v", index, err)
-				return
-			}
-
-			respCh <- idInt
-		}(idx, conn)
-	}
-
-	leaderIndex := -1
-	for {
-		idx := <-respCh
-		if idx > 0 {
-			leaderIndex = idx
-			break
-		}
-	}
-
-	logger.Logger.Infof("action: gateway_leader_found | gateway%d", leaderIndex)
-
-	conn, ok := conns[leaderIndex]
-	if !ok {
-		return nil, fmt.Errorf("leader gateway %d not found in connections", leaderIndex)
-	}
-	return conn, nil
+	return nil, fmt.Errorf("could not connect to any gateway")
 }
