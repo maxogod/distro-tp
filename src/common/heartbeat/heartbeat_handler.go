2a872e53646750f009f66c1778a8aaa8176e5555
maxogod <maxi18wars@gmail.com>
2025-12-03 12:11:59 -0300

    chore: remove unused bully election

--- src/common/heartbeat/heartbeat_handler.go
+++ src/common/heartbeat/heartbeat_handler.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"sync/atomic"
 	"time"
 
 	"github.com/maxogod/distro-tp/src/common/logger"
@@ -40,23 +39,6 @@ func NewHeartBeatHandler(host string, port int, interval time.Duration) HeartBea
 	return h
 }
 
-func NewListeningHeartBeatHandler(host string, port int, interval time.Duration) (HeartBeatHandler, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	h := &heartbeatHandler{
-		host:     host,
-		port:     port,
-		interval: interval,
-
-		ctx:    ctx,
-		cancel: cancel,
-	}
-	err := h.startListening(host, port)
-	if err != nil {
-		return nil, err
-	}
-	return h, nil
-}
-
 func (h *heartbeatHandler) StartSending() error {
 	addr := fmt.Sprintf("%s:%d", h.host, h.port)
 	udpAddr, err := net.ResolveUDPAddr("udp", addr)
@@ -72,62 +54,13 @@ func (h *heartbeatHandler) StartSending() error {
 	return nil
 }
 
-func (h *heartbeatHandler) StartSendingToAll(destinationAddrs []string) error {
-	for _, addr := range destinationAddrs {
-		udpAddr, err := net.ResolveUDPAddr("udp", addr)
-		if err != nil {
-			continue
-		}
-		conn, err := net.DialUDP("udp", nil, udpAddr)
-		if err != nil {
-			continue
-		}
-		go h.sendAtIntervals(conn)
-	}
-	return nil
-}
-
-func (h *heartbeatHandler) StartReceiving(onTimeoutFunc func(params any), timeoutAmount time.Duration) error {
-	go h.receiveHeartbeatsWithTimeout(onTimeoutFunc, timeoutAmount)
-	return nil
-}
-
-func (h *heartbeatHandler) Stop() {
-	if h.cancel != nil {
-		h.cancel()
-		h.ctx, h.cancel = context.WithCancel(context.Background())
-	}
-}
-
 func (h *heartbeatHandler) Close() {
 	if h.cancel != nil {
 		h.cancel()
-		h.cancel = nil
-	}
-	if h.conn != nil {
-		h.conn.Close()
-		h.conn = nil
 	}
 }
 
 // ------------ Private Methods ------------
-
-func (h *heartbeatHandler) startListening(host string, port int) error {
-	h.host = host
-	h.port = port
-
-	addr := fmt.Sprintf("%s:%d", h.host, h.port)
-	udpAddr, err := net.ResolveUDPAddr("udp", addr)
-	if err != nil {
-		return err
-	}
-	conn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		return err
-	}
-	h.conn = conn
-	return nil
-}
 
 func (h *heartbeatHandler) sendAtIntervals(conn *net.UDPConn) {
 	ticker := time.NewTicker(h.interval)
@@ -163,45 +96,4 @@ func (h *heartbeatHandler) sendHeartbeat(conn *net.UDPConn) error {
 	conn.Write(data)
 
 	return nil
-}
-
-func (h *heartbeatHandler) receiveHeartbeatsWithTimeout(onTimeoutFunc func(params any), timeoutAmount time.Duration) {
-	buf := make([]byte, BUFFER_SIZE)
-	var heartbeatCounter atomic.Int64
-
-	timeoutTimer := time.NewTimer(timeoutAmount)
-	defer timeoutTimer.Stop()
-
-	for {
-		select {
-		case <-h.ctx.Done():
-			return
-		case <-timeoutTimer.C:
-			onTimeoutFunc(int(heartbeatCounter.Load()))
-			return
-		default:
-			h.conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond)) // This is to not block indefinitely
-			n, _, err := h.conn.ReadFromUDP(buf)
-
-			if err != nil {
-				if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-					continue
-				}
-				select {
-				case <-h.ctx.Done():
-					return // Exit if context is done
-				default:
-					logger.Logger.Errorf("Error receiving heartbeat: %v", err)
-					return
-				}
-			}
-			// Process heartbeat
-			var hb protocol.HeartBeat
-			if err := proto.Unmarshal(buf[:n], &hb); err != nil {
-				continue
-			}
-			heartbeatCounter.Add(1)
-			timeoutTimer.Reset(timeoutAmount)
-		}
-	}
 }
--- src/common/heartbeat/interface.go
+++ src/common/heartbeat/interface.go
@@ -1,7 +1,5 @@
 package heartbeat
 
-import "time"
-
 // HeartBeatHandler is responsible for sending heartbeat signals to
 // a specified host and port at regular intervals.
 type HeartBeatHandler interface {
@@ -9,17 +7,6 @@ type HeartBeatHandler interface {
 	// StartSending initiates the heartbeat sending process in a routine.
 	StartSending() error
 
-	// StartSendingToAll initiates the heartbeat sending process to multiple hosts.
-	StartSendingToAll(destinationAddrs []string) error
-
-	// StartReceiving initiates the heartbeat receiving process in a routine.
-	// The amount of heartbeats received is passed to the onTimeoutFunc when a timeout occurs.
-	// This is used for testing purposes to see how many heartbeats were received before timing out.
-	StartReceiving(onTimeoutFunc func(params any), timeoutAmount time.Duration) error
-
 	// Close finishes the heartbeat sending process and cleans up resources.
 	Close()
-
-	// Stop halts the heartbeat sending or receiving process, allowing for a potential restart.
-	Stop()
 }
